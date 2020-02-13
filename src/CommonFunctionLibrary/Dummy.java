e605b85e7acfef99bf65e48159d84a89623b148b
madhuch <madhuch@ojt18-PC.qedge.com>
2020-02-13 12:04:04 +0530

    Initial commit

---
+++ src/CommonFunctionLibrary/Dummy.java
@@ -0,0 +1,14 @@
+package CommonFunctionLibrary;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/campaign/landing.php?campaign_id=1653993517&extra_1=s%7Cc%7C318504235901%7Ce%7Cfacebook%7C&placement=&creative=318504235901&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D1653993517%26adgroupid%3D63066387003%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D1t1%26target%3D%26targetid%3Dkwd-541132862%26loc_physical_ms%3D1007740%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMIp7KAvp6o5wIVjYmPCh3zsgebEAAYASAAEgJfBvD_BwE");
+		
+
+	}
+
+}
